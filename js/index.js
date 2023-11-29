89b2897d3baea8d814e57007c0efd510dfd14d27
Abdulbas1d <muhammadaliyevabdulbosid@gmail.com>
2023-11-30 00:25:58 +0500

    all examples are solved

--- js/index.js
+++ js/index.js
@@ -41,6 +41,23 @@
 
 // 4-Misol
 // let a = 5;
+// let b = 4;
+// let c = 8;
+// let min;
+
+// if (a<b) {
+//     min=a
+// }else{
+//     min=b
+// }
+// if (min>c) {
+//     min=c
+// }
+// console.log(min ** 3);
+
+
+// 5-Misol
+// let a = 5;
 // let b = 6;
 // let c = 9;
 // let d = 18;
@@ -66,4 +83,118 @@
 // console.log(max2);
 
 
-// 5-Misol
+// 6-Misol
+// let a = 5;
+// let b = 7;
+// let c = 9;
+// let middle;
+
+// if (a > b) {
+//     middle = a
+// } else{
+//     middle = b
+// }
+// if (middle > c) {
+//     middle = c
+// };
+// console.log(middle);
+
+
+// 7-Misol
+// let a = ((12/10)-12%10/10)+12%10;
+// let b = ((36/10)-36%10/10)+36%10;
+// let c = ((28/10)-28%10/10)+18%10;
+// let big;
+
+// if (a>b) {
+//     big=a
+// }else{
+//     big=b
+// }
+// if (big<c) {
+//     big=c
+// };
+// console.log(big);
+
+
+// 8-Misol
+// let a = (12/3==0);
+// let b = (10/3==0);
+// let c =  (8/3==0);
+// let d = (19/3==0);
+// let e =  (6/3==0);
+// let answer;
+
+// if (a) {
+//     answer = "rost"
+// }else {
+//     answer = "yolgon"
+// };
+// if (b) {
+//     answer = 'rost'
+// } else{
+//     answer = "yolgon"
+// };
+// if (c) {
+//     answer = 'rost'
+// } else{
+//     answer = "yolgon"
+// };
+// if (d) {
+//     answer = 'rost'
+// } else{
+//     answer = "yolgon"
+// };
+// if (e) {
+//     answer = 'rost'
+// } else{
+//     answer = "yolgon"
+// };
+// console.log(answer);
+
+
+// 9-Misol
+// let b = (10/3==0);
+// let c =  (8/3==0);
+// let d = (19/3==0);
+// let e =  (6/3==0);
+// let answer;
+// if (b) {
+//     answer = 'rost'
+// } else{
+//     answer = "yolgon"
+// };
+// if (c) {
+//     answer = 'rost'
+// } else{
+//     answer = "yolgon"
+// };
+// if (d) {
+//     answer = 'rost'
+// } else{
+//     answer = "yolgon"
+// };
+// if (e) {
+//     answer = 'rost'
+// } else{
+//     answer = "yolgon"
+// };
+// console.log(answer);
+
+
+
+// 10-Misol
+// let a = ((45/10)-45%10/10)+45%10;
+// let b = ((21/10)-21%10/10)+21%10;
+// let d = ((38/10)-38%10/10)+38%10;
+// let max;
+
+// if (a>b) {
+//     max = a
+// }else {
+//     max = b
+// }
+// if (max<d) {
+//     max = d
+// };
+// console.log(max);
