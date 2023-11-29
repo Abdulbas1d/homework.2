5ee11ff4871363db669179b2ffc734260bc6bbf3
Abdulbas1d <muhammadaliyevabdulbosid@gmail.com>
2023-11-29 22:49:01 +0500

    Examples 1 to 4 have been developed

---
+++ js/index.js
@@ -0,0 +1,69 @@
+// 1-Misol
+// let a = (45/10)-45%10/10;
+// let b = (56/10)-56%10/10;
+// let max;
+
+// if (a > b) {
+//     max=a
+// }else{
+//     max=b
+// };
+// console.log(max);.
+
+
+// 2-Misol
+// let a = (345/100)-345%100/100;
+// let b = (345%100)-345%10;
+// let c = (345%10);
+// let min;
+
+// if(a<b){
+//     min=a
+// }else{
+//     min=b
+// }
+// if(min<c){
+//     min=c
+// };
+
+// console.log(min);
+
+
+// 3-Misol
+// let a = ((345/10)-345%10/10)-(((345/100)-345%100/100)*10);
+// let res
+// if (a/3==0) {
+//     res='Rost'
+// }else{
+//     res='Yolgon'
+// };
+
+
+// 4-Misol
+// let a = 5;
+// let b = 6;
+// let c = 9;
+// let d = 18;
+// let max;
+// let max1;
+// let max2;
+
+// if (a>b){
+//     max=a
+// }else{
+//     max=b
+// }
+// if (c>d) {
+//     max1=c
+// }else{
+//     max1=d
+// }
+// if(max>max1) {
+//     max2=max
+// }else{
+//     max2=max1
+// };
+// console.log(max2);
+
+
+// 5-Misol
